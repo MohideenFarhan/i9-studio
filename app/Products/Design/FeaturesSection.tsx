7884cbdf7621985609cebb56098e085dbb2cbc43
MohideenFarhan <mohideenfarhan12345@gmail.com>
2025-03-25 11:07:04 +0530

    Updated feature

--- app/Products/Design/FeaturesSection.tsx
+++ app/Products/Design/FeaturesSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 import CTASection from "./CTASection";
 import VideoComponent from "../Design/VideoComponent";
 
