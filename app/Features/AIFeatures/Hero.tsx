39f4b6e174557503f74eaec8a8ddb9f679d8a8a7
MohideenFarhan <mohideenfarhan12345@gmail.com>
2025-03-28 15:40:43 +0530

    Updated feature

--- app/Features/AIFeatures/Hero.tsx
+++ app/Features/AIFeatures/Hero.tsx
@@ -17,8 +17,8 @@ const AIFeatures = () => {
                 className="relative flex flex-col items-start min-h-screen w-full overflow-hidden text-white px-4"
                 style={{ background: "radial-gradient(circle, rgba(125,131,187,1) 0%, rgba(32,29,78,1) 34%, rgba(149,149,178,1) 100%)" }}
             >
-                <div className="w-full mt-30 sm:mt-30 md:mt-80 mx-2 md:mx-10 max-w-7xl text-left">
-                    <h1 className="text-3xl sm:text-5xl lg:text-8xl md:text-7xl font-extrabold leading-tight">
+                <div className="w-full mt-30 sm:mt-30 md:mt-80 mx-2 md:mb-10  md:mx-10 max-w-7xl text-left">
+                    <h1 className="text-3xl sm:text-5xl lg:text-7xl md:text-7xl font-extrabold leading-tight">
                         Revolutionize teamwork <br />
                         with AI-powered efficiency.
                     </h1>
